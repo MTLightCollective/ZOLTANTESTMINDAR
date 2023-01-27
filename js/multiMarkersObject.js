e33897509bcbfeb0c78f675075459e44c223e622
MTLight <50854957+MTLightCollective@users.noreply.github.com>
2023-01-27 09:05:07 -0500

    Create multiMarkersObject.js
    Adding a js script to display 3d objects to multiple markers. 
    The script comes from vsigno and is adapted by chatgpt.
    I still have to adapt it to our current app

---
+++ js/multiMarkersObject.js
@@ -0,0 +1,53 @@
+AFRAME.registerComponent('markers_start',{
+	init:function(){
+		console.log('Add markers to the scene');
+
+		var sceneEl = document.querySelector('a-scene');
+		
+		//list of the markers
+		for(var i=1; i<19; i++)
+		{
+			var url="resources/markers/pattern-Individual_Blocks-"+i+".patt";
+			markersURLArray.push(url);
+			markersNameArray.push('Marker_'+i);
+			//console.log(url);
+		}
+
+		for(var k=0; k<18; k++)
+		{
+			var markerEl = document.createElement('a-marker');
+			markerEl.setAttribute('type','pattern');
+			markerEl.setAttribute('url',markersURLArray[k]);
+			markerEl.setAttribute('id',markersNameArray[k]);
+
+			markerEl.setAttribute('registerevents','');
+			sceneEl.appendChild(markerEl);
+
+			//Adding 3D object to each marker
+			var objectEl = document.createElement('a-entity');
+			
+			objectEl.setAttribute('id','object');
+			objectEl.setAttribute('obj-model', {obj: 'url(assets/models/object.obj)', mtl: 'url(assets/models/object.mtl)'});
+			objectEl.object3D.position.set(0, 0, 0);
+			objectEl.object3D.rotation.set(0, 0, 0);
+
+			markerEl.appendChild(objectEl);
+		}
+	}
+});
+
+AFRAME.registerComponent('registerevents', {
+		init: function () {
+			const marker = this.el;
+
+			marker.addEventListener("markerFound", ()=> {
+				var markerId = marker.id;
+				console.log('Marker Found: ', markerId);
+			});
+
+			marker.addEventListener("markerLost",() =>{
+				var markerId = marker.id;
+				console.log('Marker Lost: ', markerId);
+			});
+		},
+});
